4cdaedeb467601806f8057d8e5b6c42d1660693e
fatisenko <fatihsenko@gmail.com>
2022-02-14 18:33:03 +0300

    Restars Button added

--- app.js
+++ app.js
@@ -4,38 +4,41 @@ const inputArea = document.getElementsByClassName("input")[0];
 const checkButton = document.querySelector(".submit");
 const attempNumber = document.querySelector(".attemp");
 const winner = document.querySelector(".winner");
+const restart = document.querySelector(".restart");
 let minNum = 1;
 let maxNum = 100;
 let c = 8;
 
-
-
-    console.log("hi");
     const randomNumber = Math.floor(Math.random() * 100) + 1;
             checkButton.addEventListener("click",()=>{
-            console.log(randomNumber);
             if (c == 0) {
-                winner.innerHTML = "Game Over! Try again...";
+                winner.innerHTML = "Game Over! Try again";
                 winner.style.color = "red";
                 checkButton.disabled = true;
                 inputArea.disabled = true;
 
             }else if (+inputArea.value == randomNumber) {
-               winner.innerHTML = "Congrats!";
+               winner.innerHTML = "Congrats! You Find The Number";
                 checkButton.disabled = true;
                 inputArea.disabled = true;
 
-            }else if (+inputArea.value > randomNumber){
+            }else if (+inputArea.value > randomNumber && +inputArea.value < maxNum){
+                c--;
+                attempNumber.innerHTML = c;
                 maxNum = +inputArea.value;
                 guessNumber.innerHTML = `(${minNum} - ${maxNum})`;
+                
+            }else if (+inputArea.value < randomNumber && +inputArea.value > minNum){
                 c--;
                 attempNumber.innerHTML = c;
-            }else{
                 minNum = +inputArea.value;
                 guessNumber.innerHTML = `(${minNum} - ${maxNum})`;
-                c--;
-                attempNumber.innerHTML = c;
+                
             };
+        });
+        restart.addEventListener("click", ()=>{
+            window.location.reload(false);
         });
 
 }
+
